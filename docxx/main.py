7c586c80c57e8c53878a995591a1fec7a0c1f84f
csomePro <csome@m.scnu.edu.com>
2020-12-14 08:59:32 +0800

    MainCode

--- docxx/main.py
+++ docxx/main.py
@@ -6,7 +6,7 @@ import os
 path = r"js01.docx"
 document = Document(path)
 kk = 0
-# isp = 0
+isp = 0
 q = []
 mq = []
 # a = []
@@ -17,36 +17,57 @@ for paragraph in document.paragraphs:
         kk += 1
         spr = paragraph.text
         s += spr + "\n"
-        if re.search("[0-9]、", s) is not None and "D、" in spr:
-            q.append(s)
-            rrra = re.search("（ [A-Z] ）", s)
-            if rrra is not None:
-                rra = rrra.group(0)
-                ra.append(rra)
-                mq.append(s.replace(rra, "（  ）"))
-            s = ""
+        if re.search("[0-9]、", s) is not None:
+            if re.search("[0-9]、", spr) is not None:
+                s = spr + "\n"
+                isp = 0
+            if "A" in spr:
+                isp += 1
+            if "B" in spr:
+                isp += 1
+            if "C" in spr:
+                isp += 1
+            if "D" in spr:
+                isp += 1
+            if isp >= 5:
+                print(s)
+                print()
+                q.append(s)
+                rrra = re.search("[A-Z]", s)
+                if rrra is not None:
+                    rra = rrra.group(0)
+                    ra.append(rra)
+                    mq.append(s.replace(rra, "", 1))
+                s = ""
+                isp = 0
 
 mode = int(input("1.you choose the question's index\n"
-                 "2.auto random question's index\n"))
+                 "2.auto random question's index\n"
+                 "3.one by one test from 1 to end\n"))
 op = 0
 ansIndex = 0
 rightIndex = 0
+i = 0
+index = 0
 while(1):
     if mode == 1:
         i = int(input("question(1- {kk}):".format(kk=len(mq))))
     elif mode == 2:
         i = randint(1, len(mq))
+    elif mode == 3:
+        index += 1
+        i = index
     else:
         print("Unknown")
         break
     i -= 1
-    if i < 0 or i > len(mq):
+    if i < 0 or i >= len(mq):
         break
     print(mq[i])
     ans = input("answer:")
     if ans in "-1":
         break
-    if ans.upper() in ra[i]:
+    if ans.upper() == ra[i]:
         print("right!!")
         rightIndex += 1
     else:
