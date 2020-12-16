be8157e79791b8fe0531a2003838f0869f190d29
csomePro <csome@m.scnu.edu.com>
2020-12-16 12:59:23 +0800

    MainCode

--- docxx/main02.py
+++ docxx/main02.py
@@ -1,15 +1,62 @@
 from random import randint
-import re
 from docx import Document
+import re
 import os
+import json
 
-path = r"js01.docx"
+init = 1
+path = r"js02.docx"  # need to save
+ansIndex = 0  # need to save
+rightIndex = 0  # need to save
+i = 0  # need to save
+index = 0  # need to save
+isansmq = []  # need to save
+mode = 1  # need to save
+x = 1  # need to save
+
+if not os.path.exists(r'data.json'):
+    print("Initializing...")
+    while 1:
+        path = input("Enter your Question bank filename or file path:")
+        if os.path.exists(path):
+            break
+        else:
+            print("'{file}' does not exist, please try again.".format(file=path))
+    with open(r'data.json', 'w') as f:
+        data = {'path': path, 'init': 1}
+        f.write(json.dumps(data))
+
+else:
+    inp1 = input("Do you want to load your data(y/n):")
+    if inp1 is 'y' or inp1 is 'Y':
+        with open(r'data.json', 'r') as f:
+            data = json.loads(f.read())
+            path = data['path']
+            init = data['init']
+            if init == 0:
+                ansIndex = data['ansIndex']
+                rightIndex = data['rightIndex']
+                i = data['i']
+                index = data['index']
+                isansmq = data['isansmq']
+                mode = data['mode']
+                x = data['x']
+        if init == 1:
+            print("You have not saved, just start initially.")
+    else:
+        inp2 = input("Do you want to change Question bank(y/n):")
+        if inp2 is 'y' or inp2 is 'Y':
+            while 1:
+                path = input("Enter your Question bank filename or file path:")
+                if os.path.exists(path):
+                    break
+                else:
+                    print("'{file}' does not exist, please try again.".format(file=path))
+
 document = Document(path)
 kk = 0
 isp = 0
-# q = []
 mq = []
-# a = []
 ra = []
 s = ""
 miniment = 0
@@ -90,27 +137,23 @@ for paragraph in document.paragraphs:
 # for i in mq:
 #     print(i)
 #     print("@\n")
-
-isansmq = [0 for i in range(len(mq))]
-
-mode = int(input("1.you choose the question's index\n"
-                 "2.auto random question's index\n"
-                 "3.one by one test from x to end\n"))
-op = 0
-ansIndex = 0
-rightIndex = 0
-i = 0
-index = 0
-while mode == 3:
-    index = int(input(f"input x(1-{{indexx}}):".format(indexx=len(mq))))
-    x = index
-    if index < 1 or index > len(mq):
-        print("{indexx} in illegal".format(indexx=index))
-        continue
-    else:
-        break
+if init == 1:
+    isansmq = [0 for i in range(len(mq))]  # need to save
+    mode = int(input("1.you choose the question's index\n"
+                     "2.auto random question's index\n"
+                     "3.one by one test from x to end\n"))  # need to save
+    while mode == 3:
+        index = int(input(f"input x(1-{{indexx}}):".format(indexx=len(mq))))
+        x = index
+        if index < 1 or index > len(mq):
+            print("{indexx} in illegal".format(indexx=index))
+            continue
+        else:
+            break
 
 # print(len(mq))
+save = 1  # 判断是否是非法跳出，或是刷题结束
+
 while 1:
     if mode == 1:
         i = int(input("question(1 - {kk}):".format(kk=len(mq))))
@@ -120,8 +163,9 @@ while 1:
         # print("i:", end="")
         # print(i)
         if 0 not in isansmq:
+            save = 0
             break
-        if isansmq[i-1] == 1:
+        if isansmq[i - 1] == 1:
             continue
         # print(isansmq)
 
@@ -129,19 +173,22 @@ while 1:
         i = index
         index += 1
         index = (index - x) % (len(mq) - x + 1) + x
-        if 0 not in isansmq[x-1:]:
+        if 0 not in isansmq[x - 1:]:
+            save = 0
             break
         if isansmq[i - 1] == 1:
             continue
         # print(isansmq)
     else:
         print("Unknown")
+        save = 0
         break
 
     i -= 1
     if i < 0 or i >= len(mq):
+        save = 0
         break
-    print("第{ii}题".format(ii=i+1))
+    print("第{ii}题".format(ii=i + 1))
     print(mq[i])
     ans = input("answer(input 'quit' to quit):")
     if ans == "quit":
@@ -153,9 +200,27 @@ while 1:
     else:
         print("wrong")
     ansIndex += 1
-    op = int(float(rightIndex / ansIndex) * 10000 + 0.5) / 100
+    tmp = int(float(rightIndex / ansIndex) * 10000 + 0.5) / 100
     print("正确答案：" + ra[i])
-    print("正确率：{x}%  {rights}/{anss}".format(x=op,anss=ansIndex,rights=rightIndex))
+    print("正确率：{x}%  {rights}/{anss}".format(x=tmp, anss=ansIndex, rights=rightIndex))
     print()
+
+if save == 1:
+    if input("Do you want to save(y/n)") is 'y':
+        with open(r'data.json', 'w') as f:
+            data = {'init': 0,
+                    'path': path,
+                    'ansIndex': ansIndex,
+                    'rightIndex': rightIndex,
+                    'i': i,
+                    'index': index - 1,
+                    'isansmq': isansmq,
+                    'mode': mode,
+                    'x': x}
+            f.write(json.dumps(data))
+else:
+    with open(r'data.json', 'w') as f:
+        data = {'path': path, 'init': 1}
+        f.write(json.dumps(data))
 
 os.system("pause")
